6dc01b7ab2090bb5a36ff16be5c44e4bb40006dc
HacenMario <stevenhacen@gmail.com>
2026-08-12 12:25:24 +0100

    Update CACHE_NAME to version v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='anti-sleep-v2-shell-v2';
+const CACHE_NAME='anti-sleep-v2-shell-v3';
 const APP_SHELL=['./','./index.html','./manifest.json','./i18n.js','./favicon.png','./icons/favicon.png','./Algeria%20Heart%20Flag.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
